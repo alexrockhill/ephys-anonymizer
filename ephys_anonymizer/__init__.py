8c0996e080bfa3b10dab25be5071c7b4f8d8c52f
Alex <aprockhill206@gmail.com>
2020-08-16 17:52:29 -0700

    change version

--- ephys_anonymizer/__init__.py
+++ ephys_anonymizer/__init__.py
@@ -1,6 +1,6 @@
 """A anonymization toolbox for video and neuroimaging files."""
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
 from ephys_anonymizer.anonymizer import video_anonymize, raw_anonymize  # noqa
